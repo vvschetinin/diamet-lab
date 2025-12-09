e1ffde4d5f2dc301aa56ebffe85475b3637f8b02
Vladlen Schetinin <born-1965@mail.ru>
2025-12-09 23:05:00 +0300

    Чистка TS

--- src/assets/ts/app.ts
+++ src/assets/ts/app.ts
@@ -1,8 +1,1 @@
-import "@/assets/ts/components/inputmask/main";
-import "@/assets/ts/components/cookie/main";
-import "@/assets/ts/components/formsend/formsend";
-import "@/assets/ts/components/scroollmenu/main";
-import "@/assets/ts/components/articles/main";
 import "@/assets/ts/main/script";
-
-// import "@/assets/ts/components/lazyvideo/lazy-video";  //  Обязательно должен загружаться последжним
